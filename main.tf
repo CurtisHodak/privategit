3991a2369bf8b002031d6a10dd7ae4282b598cf1
Curtis Hodak <curtish@spacelift.com>
2025-07-30 15:42:23 -0400

    fix providers and main

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = "1.5.7"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
 resource "aws_key_pair" "gitlab_key" {
   key_name   = "gitlab-key"
   public_key = file(var.public_key_path)

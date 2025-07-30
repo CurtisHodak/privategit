e6905ef4d7fb1a449868f779fe593442b903fef8
Curtis Hodak <curtish@spacelift.com>
2025-07-30 16:58:28 -0400

    fixed docker stuff

--- user_data.sh
+++ user_data.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 # Update system and install Docker
-yum update -y
-amazon-linux-extras install docker -y
-systemctl enable docker
-systemctl start docker
+#Create the public key file first 
+# ssh-keygen -t rsa -b 4096 -C "gitlab-ec2-access" -f ~/.ssh/gitlabkey
+Create a variable in your stack 
+# ssh -i ~/.ssh/gitlabkey ec2-user@<public-ip>
+sudo yum update -y
+sudo yum install docker -y
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo usermod -aG docker ec2-user
+# exit and reconnect to apply group changes
 
 # Create GitLab directories
 mkdir -p /srv/gitlab/config /srv/gitlab/logs /srv/gitlab/data
 
 # Run GitLab CE Docker container
 docker run --detach \
-  --hostname $(curl -s http://169.254.169.254/latest/meta-data/public-hostname) \
-  --publish 443:443 --publish 80:80 --publish 22:22 \
+  --hostname "$(curl -s http://169.254.169.254/latest/meta-data/public-hostname)" \
+  --publish 443:443 \
+  --publish 80:80 \
   --name gitlab \
   --restart always \
   --volume /srv/gitlab/config:/etc/gitlab \

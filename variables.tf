fe0cc5044cc28f085e6320f36aad86f433275af6
Curtis Hodak <curtish@spacelift.com>
2025-07-30 15:26:32 -0400

    First push

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+variable "ami_id" {
+  description = "Amazon Linux 2 AMI (HVM), SSD Volume Type"
+  default     = "ami-04505e74c0741db8d" # Change if using another region
+}
+
+variable "public_key_path" {
+  description = "Path to the public SSH key"
+  default     = "~/.ssh/id_rsa.pub"
+}

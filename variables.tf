5f543c7162b5d8ddb106024c6a5c6682e4ac01a5
Curtis Hodak <curtish@spacelift.com>
2025-07-30 15:59:56 -0400

    fix

--- variables.tf
+++ variables.tf
@@ -1,7 +1,3 @@
-variable "aws_region" {
-  default = "us-east-2"
-}
-
 variable "ami_id" {
   description = "Amazon Linux 2 AMI (HVM), SSD Volume Type"
   default     = "ami-08ca1d1e465fbfe0c" # Change if using another region
